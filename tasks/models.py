fcd0cd6bf7f2d349f605dea0bcba45a52b5e1cd6
saira <guevarax72@gmai.com>
2023-11-22 12:28:28 -0500

    proyecto de django

---
+++ tasks/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.title + ' - ' + self.user.username
+
